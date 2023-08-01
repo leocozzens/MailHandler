0d71a594bfab5ca56422286c9c8d94cc249a60dd
Leo Cozzens <leo@narthexnetworks.com>
2023-07-31 22:56:30 -0700

    Formatting changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,8 @@ public class Main {
     private static final String interfaceTitle = "MailHandler";
     private static final String internalIconPath = "images/icon.png";
     private static final double[] screenSize = ScreenManager.getScreenDim();
-    private static final int interfaceWidth = (int) (screenSize[0]*0.55);
-    private static final int interfaceHeight = (int) (screenSize[1]*.45);
+    private static final int interfaceWidth = (int) (screenSize[0]*0.55F);
+    private static final int interfaceHeight = (int) (screenSize[1]*0.45F);
     public static void main(String[] args) {
         Interface graphInterface = new Interface(interfaceTitle, interfaceWidth, interfaceHeight, internalIconPath, Main.class);
         graphInterface.run();
